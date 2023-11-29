3be6804b8f0287efc96e2169205ec58a8506bd3c
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-11-29 17:34:21 -0300

    ADD exercise

---
+++ Secao 1 - Conceitos iniciais/src/main/kotlin/Exercicio.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var nomeCompleto: String = "Pedro Henrique Siqueira"
+
+    var texto: String
+
+    val numeroTenis: UByte = 41u
+
+    val pibBrasil: Long = 1869000000000
+
+    val populacaoBrasil: Int = 211000000
+
+    println(nomeCompleto)
+    println("PIB per Capita Brasileiro: ${ pibBrasil / populacaoBrasil }")
+}
