c58dd83c1aeafc72e1a33e1b8939c504ce39ac43
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-12-09 11:55:41 -0300

    ADD exercism

--- Section 3 - flux control/src/main/kotlin/ExercicioFinal.kt
+++ Section 3 - flux control/src/main/kotlin/ExercicioFinal.kt
@@ -1,20 +1,38 @@
 fun main() {
+    ex4(10)
+}
+
+fun ex1() {
     for (i in 1..50) {
         if (i % 5 == 0) {
             continue
         }
         print("$i ")
     }
+}
 
+fun ex2() {
     for (i in 50 downTo 1) {
         print("$i ")
     }
+}
 
+fun ex3() {
     var j = 0
-    for (i in 1..500){
+    for (i in 1..500) {
         j += i
     }
 
     println("$j")
+}
+fun ex4(n: Int){
+    var contador = 0
+    for (i in 1..n){
+        contador = i
 
+        for (n in 1..contador){
+            print("#")
+        }
+        println()
+    }
 }
